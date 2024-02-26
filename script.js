8a42acf15d5ccee3c787d09294324bd55b3e58a8
Farhan Akhtar <104159043+f-akhtar@users.noreply.github.com>
2024-02-27 00:20:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
-            msg.innerHTML = "Message Send Successfully"
+            msg.innerHTML = "Message Sent Successfully"
             setTimeout(function () {
                 msg.innerHTML = ""
             }, 5000)
